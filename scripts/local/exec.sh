ce169eb1fb4d3a13f7eb2b318392c0307786aba1
adelphi-liong <adelphi@atomi.cloud>
2026-01-23 07:42:00 -0800

    fix(default): upgrade to use newest docker client
    - updated ip_address from 152.42.167.240 to 178.128.84.148
    - added new ssh key for user ernest
    
    upgrading to the latest docker client requires updating the IP address and
    adding a new SSH key for secure access.

--- scripts/local/exec.sh
+++ scripts/local/exec.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 cluster="$1"
+key="$2"
 
 [ -z "$cluster" ] && cluster="$(printf "mica\ntalc\nopal\nruby\ntopaz\namber\n" | fzf)"
-
+[
+[ -z "$key" ] && key="$(ls ~/.ssh/id* | grep -v '.pub' | fzf)"
 set -euo pipefail
 
 echo "🚪 Entering SSH session..."
 json="$(tofu "-chdir=tofu/live/$cluster" output -json)"
 address=$(echo "$json" | jq -r '.address.value')
-ssh "kirin@$address"
+ssh -i "$key" "kirin@$address"
