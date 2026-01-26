79154421ab32dee830001fe4fc8c7c5f6bdb7bcb
adelphi-liong <adelphi@atomi.cloud>
2026-01-26 15:30:16 -0800

    feat: allow key selection during deploy

--- scripts/cd/deploy.sh
+++ scripts/cd/deploy.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 cluster="$1"
+key="$2"
 
 [ -z "$cluster" ] && cluster="$(printf "mica\ntalc\nopal\nruby\ntopaz\namber\n" | fzf)"
+[ -z "$key" ] && key="$(find ~/.ssh -maxdepth 1 -name 'id_*' ! -name '*.pub' -print | fzf)"
 
 set -euo pipefail
 
 echo "🚀 Deploying..."
 json="$(tofu "-chdir=tofu/live/$cluster" output -json)"
 address=$(echo "$json" | jq -r '.address.value')
-ansible-playbook playbook.yaml -i "kirin@$address,"
+ansible-playbook playbook.yaml -i "kirin@$address," --private-key "$key"
 echo "✅ Deployment successful"
